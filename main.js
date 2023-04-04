81b28a19fc5da6b797f5f3676ba7e244d4763ea2
Lousio-n <55360932+Lousio-n@users.noreply.github.com>
2023-04-04 23:36:25 +0200

    add function to buttons

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+const numbers = document.querySelectorAll(".number-input");
+const operators = document.querySelectorAll(".operator-input");
+const equals = document.getElementById("equals");
+const input = document.getElementById("calculation-input");
+const output = document.getElementById("calculation-output");
+const resetButton = document.getElementById("reset-button");
+const undoButton = document.getElementById("undo-button");
 
+let operatorToUse;
 
-function operate(newNum, lastNum) {
+const numberClicked = numbers.forEach(number => {
+  number.addEventListener("click", (e) => {
+    input.textContent += e.target.textContent;
+  });
+});
 
-}
+const operatorClicked = operators.forEach(operator => {
+  operator.addEventListener("click", (e) => {
+    operatorToUse = e.target.textContent;
+  });
+});
+
+equals.addEventListener("click", () => {
+});
