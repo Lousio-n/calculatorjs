a3fcbdef8bc1adb8c8296c2aabe24e49083e4673
Lousio-n <55360932+Lousio-n@users.noreply.github.com>
2023-04-05 22:11:56 +0200

    create basic calculator function and add reset/undo

--- main.js
+++ main.js
@@ -7,18 +7,62 @@ const resetButton = document.getElementById("reset-button");
 const undoButton = document.getElementById("undo-button");
 
 let operatorToUse;
+let operatorClickedStatus = false;
+let numberA = "";
+let numberB = "";
+let calculatedNumber;
+
+function clearInputOutput() {
+  output.textContent = "";
+  input.textContent = "";
+  operatorClickedStatus = false;
+  numberA = "";
+  numberB = "";
+};
+
+function clearInput() {
+  input.textContent = "";
+  operatorClickedStatus = false;
+  numberA = "";
+  numberB = "";
+}
+
+function operate(a, b) {
+  if (operatorToUse === '+') return a + b;
+  if (operatorToUse === '-') return a - b;
+  if (operatorToUse === '*') return a * b;
+  if (operatorToUse === '/') return a / b;
+}
 
 const numberClicked = numbers.forEach(number => {
   number.addEventListener("click", (e) => {
+    if (operatorClickedStatus === false) {
+      numberA += e.target.textContent;
+    } else {
+      numberB += e.target.textContent;
+    }
     input.textContent += e.target.textContent;
   });
 });
 
 const operatorClicked = operators.forEach(operator => {
   operator.addEventListener("click", (e) => {
+    operatorClickedStatus = true;
     operatorToUse = e.target.textContent;
+    input.textContent += ` ${e.target.textContent} `;
   });
 });
 
 equals.addEventListener("click", () => {
+  calculatedNumber = operate(+numberA, +numberB);
+  output.textContent = calculatedNumber;
 });
+
+resetButton.addEventListener("click", () => {
+  clearInputOutput();
+})
+
+undoButton.addEventListener("click", () => {
+  clearInput();
+})
+
