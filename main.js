1f6b0f73f6b1445e0ade260576d34928d5f50f88
Lousio-n <55360932+Lousio-n@users.noreply.github.com>
2023-04-08 23:16:37 +0200

    improve how the calculation works

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const numbers = document.querySelectorAll(".number-input");
 const operators = document.querySelectorAll(".operator-input");
 const equals = document.getElementById("equals");
-const input = document.getElementById("calculation-input");
-const output = document.getElementById("calculation-output");
+const calculatorDisplay = document.getElementById("calculator-display");
 const resetButton = document.getElementById("reset-button");
 const undoButton = document.getElementById("undo-button");
 
@@ -12,27 +11,27 @@ let numberA = "";
 let numberB = "";
 let calculatedNumber;
 
-function clearInputOutput() {
-  output.textContent = "";
-  input.textContent = "";
+function clearCalculatorDisplay() {
+  calculatorDisplay.textContent = "";
+  operatorClickedStatus = false;
+  numberA = "";
+  numberB = "";
+  calculatedNumber = undefined;
+};
+
+function clearInput() {
+  calculatorDisplay.textContent = "";
   operatorClickedStatus = false;
   numberA = "";
   numberB = "";
 };
-
-function clearInput() {
-  input.textContent = "";
-  operatorClickedStatus = false;
-  numberA = "";
-  numberB = "";
-}
 
 function operate(a, b) {
   if (operatorToUse === '+') return a + b;
   if (operatorToUse === '-') return a - b;
   if (operatorToUse === '*') return a * b;
   if (operatorToUse === '/') return a / b;
-}
+};
 
 const numberClicked = numbers.forEach(number => {
   number.addEventListener("click", (e) => {
@@ -41,28 +40,40 @@ const numberClicked = numbers.forEach(number => {
     } else {
       numberB += e.target.textContent;
     }
-    input.textContent += e.target.textContent;
+
+    console.log(numberA, numberB);
+/* 
+    if (calculatedNumber !== undefined) {
+      calculatorDisplay.textContent = "";
+      calculatedNumber = undefined;
+    }  */
+    calculatorDisplay.textContent += e.target.textContent;
   });
 });
 
 const operatorClicked = operators.forEach(operator => {
   operator.addEventListener("click", (e) => {
     operatorClickedStatus = true;
+    if (calculatedNumber !== undefined) {
+      numberA = calculatedNumber;
+      calculatorDisplay.textContent = numberA;
+    }
     operatorToUse = e.target.textContent;
-    input.textContent += ` ${e.target.textContent} `;
+    calculatorDisplay.textContent += ` ${e.target.textContent} `;
   });
 });
 
 equals.addEventListener("click", () => {
   calculatedNumber = operate(+numberA, +numberB);
-  output.textContent = calculatedNumber;
+  calculatorDisplay.textContent = calculatedNumber;
+  numberA = "";
+  numberB = "";
 });
 
 resetButton.addEventListener("click", () => {
-  clearInputOutput();
-})
+  clearCalculatorDisplay();
+});
 
 undoButton.addEventListener("click", () => {
   clearInput();
-})
-
+});
